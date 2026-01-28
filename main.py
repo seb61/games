1b4bae2b2d635a2c9bc3c70edeb9f14b14c113ca
Sebastian Desaulniers <desaulniers.sebastian@gmail.com>
2026-01-28 16:40:53 -0500

    Update main.py

--- main.py
+++ main.py
@@ -1,8 +1,12 @@
+def exit_without_saving() -> bool:
+    return input("Are you sure want to exit without saving? Yes/no: ").lower() in ("y", "yes")
+
+
 
 def main_loop():
 
     def is_valid_input(input_str):
-        return input_str in ("1", "2", "3", "4", "5", "6")
+        return input_str in ("1", "2", "3", "4", "5", "6") or input_str in ("add", "edit", "remove", "print", "exit and save", "exit without saving")
 
     while True:
         print(f"1. Add")
@@ -12,24 +16,25 @@ def main_loop():
         print(f"5. Exit and save")
         print(f"6. Exit without saving\n")
 
-        input_str = input("> ")
+        input_str = input("> ").lower()
         if not is_valid_input(input_str):
             print("ERROR: Invalid option\n")
             continue
 
-        match int(input_str):
-            case 1: # add entry
+        match input_str:
+            case "1" | "add": # add entry
                 pass
-            case 2: # edit entry
+            case "2" | "edit": # edit entry
                 pass
-            case 3: # remove
+            case "3" | "remove": # remove
                 pass
-            case 4: # print
+            case "4" | "print": # print
                 pass
-            case 5: # exit
+            case "5" | "exit and save": # exit
                 pass
-            case 6: # exit without saving
-                return
+            case "6" | "exit without saving": # exit without saving
+                if exit_without_saving():
+                    return
 
 
 def main(filename):
@@ -44,6 +49,5 @@ def main(filename):
 
 if __name__ == "__main__":
     main("games.csv")
-
 
 
