1537d2c375554dfd8017e204c351596d28c20fd2
KishawnW <196675028+ratter-exe@users.noreply.github.com>
2026-03-09 19:07:38 -0400

    feat: site
    website front-end

---
+++ site/server.js
@@ -0,0 +1,50 @@
+const http = require('http');
+const fs = require('fs');
+const path = require('path');
+
+const PORT = process.env.PORT || 3000;
+
+// MIME
+const mimeTypes = {
+  '.html': 'text/html',
+  '.css': 'text/css',
+  '.js': 'application/javascript',
+  '.png': 'image/png',
+  '.jpg': 'image/jpeg',
+  '.jpeg': 'image/jpeg',
+  '.svg': 'image/svg+xml',
+  '.ico': 'image/x-icon',
+  '.woff': 'font/woff',
+  '.woff2': 'font/woff2',
+  '.ttf': 'font/ttf'
+};
+
+const server = http.createServer((req, res) => {
+  // sanitize
+  let safePath = req.url.split('?')[0];
+  safePath = safePath.replace(/\.\./g, '');
+
+  // default to index
+  let filePath = path.join(__dirname, 'public', safePath);
+  if (safePath === '/' || safePath === '') {
+    filePath = path.join(__dirname, 'public', 'index.html');
+  }
+
+  // determine content type/ext
+  const ext = path.extname(filePath);
+  const contentType = mimeTypes[ext] || 'application/octet-stream';
+
+  fs.readFile(filePath, (err, content) => {
+    if (err) {
+      res.writeHead(404, { 'Content-Type': 'text/plain' });
+      res.end('404 Not Found');
+    } else {
+      res.writeHead(200, { 'Content-Type': contentType });
+      res.end(content);
+    }
+  });
+});
+
+server.listen(PORT, () => {
+  console.log(`Server running at http://localhost:${PORT}/`);
+});
