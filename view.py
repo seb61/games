7f3b98d8723c39f23eb2061e5fbbdc2d05c65c82
Iqbal Shahrill <iqbal.shahrill@ontariotechu.net>
2026-03-10 01:08:15 -0400

    Some Documentation

--- view.py
+++ view.py
@@ -1,6 +1,11 @@
 import tkinter as tk
 
 class appView:
+    # This class is a GUI Application for the login and registration page. The application uses tkinter for making the
+    # window. The window has a fixed size. So does the text, so they are formatted well together.
+
+    # This is the general sizing and appearance customization of the main attributes within the window application
+    # What the users see, not the functionality of the system.
     window = tk.Tk(className="Movie Application")
     window.geometry("500x400")
     window.configure(bg="#494d5f")
@@ -14,17 +19,21 @@ class appView:
     #login_title.place(anchor='center', relx=0.5, rely=0.25)
     login_title.pack(pady=10)
 
+    # Requesting username input
     username_title = tk.Label(master=login_frame, text="Username:", bg="#FFFFFF")
     username_title.pack()
     username_entry = tk.Entry(master=login_frame, width=25, border=1.5)
     username_entry.pack(pady=5)
 
+    #Requesting password input
     password_title = tk.Label(master=login_frame, text="Password:", bg="#FFFFFF")
     password_title.pack(pady=5)
     password_entry = tk.Entry(master=login_frame,width=25, show='*', border=1.5)
     password_entry.pack(pady=5)
 
+    # Login button
     login_button = tk.Button(master=login_frame, text="Login")
     login_button.pack(pady=15)
 
+    # Begin the main loop
     window.mainloop()
